fd1203d87fa67c2e2bbb50e2ddc475be06734db0
Subin <ryusb981211@gmail.com>
2024-10-07 13:43:02 +0900

    add

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -9,7 +9,7 @@ const ProductAll = () => {
   const getProducts = async () => {
     let searchQuery = query.get('q') || '';
     console.log('쿼리값', searchQuery);
-    let url = `http://localhost:5000/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/ryugotthis/shoppingmall/products?q=${searchQuery}`;
     // let url = `http://localhost:5000/products`;
     let response = await fetch(url);
     let data = await response.json();
